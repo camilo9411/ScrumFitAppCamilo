616f24855d8e2853184ec9b69303627fc6f2e584
Camilo Restrepo <camilorestrepo9411@gmail.com>
2021-11-29 23:14:38 -0500

    Initial commit from Android Studio Artifox

---
+++ app/src/main/java/com/example/scrumfitapp/ListRoutine_Exercise_Acitivty.java
@@ -0,0 +1,14 @@
+package com.example.scrumfitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListRoutine_Exercise_Acitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_routine_exercise);
+    }
+}
---
+++ app/src/main/java/com/example/scrumfitapp/Log_SignIn_Acitity.java
@@ -0,0 +1,14 @@
+package com.example.scrumfitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Log_SignIn_Acitity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_sign_in);
+    }
+}
